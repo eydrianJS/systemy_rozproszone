44cfa8b658f02fcfb8ef11f3d0d7be206831e9c8
stud-b00721 <stud-b00721@EDU-WSB.local>
2020-02-16 16:39:57 +0100

    frontend with login and nav

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './login.css'
 
 const Login = () => {
+    
+    const [value, setValue] = useState('');
+    
+    const handleChange = (event) => {
+        setValue(event.target.value);
+    }
+
+    const handleSubmit = () => {
+        alert(value);
+    }
+
     return (
-        <div className="login">
+        <form className="login" onSubmit={handleSubmit}>
             <h1 className="login-title">Zaloguj się</h1>
-            <input className="login-input" type="text" placeholder="Wpisz numer klienta lub login"/>
+            <input className="login-input" type="text" placeholder="Wpisz numer klienta lub login" value={value} onChange={handleChange}/>
             <button className="login-button" type="submit">Dalej</button>
-        </div>
+        </form>
     )
 }
 
